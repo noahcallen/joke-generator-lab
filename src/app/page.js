d4a36afc261ea10de86d383794380bf1643889f1
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2024-09-30 18:52:02 -0400

    login working

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
+'use client';
+
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+
+import { Button } from 'react-bootstrap';
+import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
+import { useAuth } from '@/utils/context/authContext';
+
 function Home() {
+  const { user } = useAuth();
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +19,11 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      Welcome to Next JS!
+      <h1>Hello {user.displayName}! </h1>
+      <p>Click the button below to logout!</p>
+      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+        Sign Out
+      </Button>
     </div>
   );
 }
